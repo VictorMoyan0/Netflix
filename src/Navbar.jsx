c5b3fdeba7083bad8659a69b7d1e2a96f71cefcd
agustingobstones-glitch <agustingobstones@gmail.com>
2025-09-27 20:09:06 -0300

    Initial commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,48 @@
+import { LineMdBell } from './assets/bell';
+import { MaterialSymbolsSearch } from './assets/search';
+import { TablerUser } from './assets/user';
+
+function Navbar() {
+  return (
+    <header>
+      <nav>
+        <div className="nav-menu-left">
+          <img src="/logo.png" alt="logo" />
+          <ul>
+            <li>
+              <a href="#">Inicio</a>
+            </li>
+            <li>
+              <a href="#">SeriesTV</a>
+            </li>
+            <li>
+              <a href="#">Peliculas</a>
+            </li>
+            <li>
+              <a href="#">Añadido Recientemente</a>
+            </li>
+            <li>
+              <a href="#">Mi Lista</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="nav-menu-right">
+          <MaterialSymbolsSearch />
+          <ul>
+            <li>
+              <a href="#">INFANTIL</a>
+            </li>
+            <li>
+              <a href="#">DVD</a>
+            </li>
+          </ul>
+          <LineMdBell />
+          <TablerUser />
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
