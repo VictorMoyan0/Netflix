cb1d7485828ec921ac41e1103dda00b9a8f38898
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-09-28 08:56:33 -0300

    version 16

--- src/assets/bell.jsx
+++ src/assets/bell.jsx
@@ -1,54 +1,22 @@
-export function LineMdBell(props) {
+export default function BellIcon(props) {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="2em"
-      height="2em"
       viewBox="0 0 24 24"
+      width={props.width || 24}
+      height={props.height || 24}
+      aria-hidden="true"
+      className={props.className || 'bell-icon'}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      role="img"
       {...props}
     >
-      <g
-        fill="none"
-        stroke="currentColor"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        stroke-width="2"
-      >
-        <path stroke-dasharray="4" stroke-dashoffset="4" d="M12 3v2">
-          <animate
-            fill="freeze"
-            attributeName="stroke-dashoffset"
-            dur="0.2s"
-            values="4;0"
-          />
-        </path>
-        <path
-          stroke-dasharray="28"
-          stroke-dashoffset="28"
-          d="M12 5c-3.31 0 -6 2.69 -6 6l0 6c-1 0 -2 1 -2 2h8M12 5c3.31 0 6 2.69 6 6l0 6c1 0 2 1 2 2h-8"
-        >
-          <animate
-            fill="freeze"
-            attributeName="stroke-dashoffset"
-            begin="0.2s"
-            dur="0.4s"
-            values="28;0"
-          />
-        </path>
-        <path
-          stroke-dasharray="8"
-          stroke-dashoffset="8"
-          d="M10 20c0 1.1 0.9 2 2 2c1.1 0 2 -0.9 2 -2"
-        >
-          <animate
-            fill="freeze"
-            attributeName="stroke-dashoffset"
-            begin="0.6s"
-            dur="0.2s"
-            values="8;0"
-          />
-        </path>
-      </g>
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M13.0002 4.07092C16.3924 4.55624 19 7.4736 19 11V15.2538C20.0489 15.3307 21.0851 15.4245 22.1072 15.5347L21.8928 17.5232C18.7222 17.1813 15.4092 17 12 17C8.59081 17 5.27788 17.1813 2.10723 17.5232L1.89282 15.5347C2.91498 15.4245 3.95119 15.3307 5.00003 15.2538V11C5.00003 7.47345 7.60784 4.55599 11.0002 4.07086V2H13.0002V4.07092ZM17 15.1287V11C17 8.23858 14.7614 6 12 6C9.2386 6 7.00003 8.23858 7.00003 11V15.1287C8.64066 15.0437 10.3091 15 12 15C13.691 15 15.3594 15.0437 17 15.1287ZM8.62593 19.3712C8.66235 20.5173 10.1512 22 11.9996 22C13.848 22 15.3368 20.5173 15.3732 19.3712C15.3803 19.1489 15.1758 19 14.9533 19H9.0458C8.82333 19 8.61886 19.1489 8.62593 19.3712Z"
+        fill="currentColor"
+      />
     </svg>
   );
 }
