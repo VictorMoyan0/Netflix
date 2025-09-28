c7621f7c2a361ccdf78c39ad2e5f29e5db9a2f1c
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-09-28 10:38:09 -0300

    version 51

--- src/assets/banner.jsx
+++ src/assets/banner.jsx
@@ -4,7 +4,7 @@ function Banner() {
     <section className="banner">
       <div className="banner-content">
         <h4 className="banner-subtitle">N SERIE</h4>
-        <h1 className="banner-title">LA CASA <span className="banner-title-red">DE</span> PAPEL</h1>
+        <h1 className="banner-title">INCONTROLABLES</h1>
         <div className="banner-buttons">
           <button className="banner-btn play">▶ Reproducir</button>
           <button className="banner-btn info">ℹ Más información</button>
