c8de6550bc3d03f6a2e507015aa7c21e52c64db3
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-09-28 10:41:21 -0300

    version 52

--- src/assets/banner.jsx
+++ src/assets/banner.jsx
@@ -3,15 +3,11 @@ function Banner() {
   return (
     <section className="banner">
       <div className="banner-content">
-        <h4 className="banner-subtitle">N SERIE</h4>
         <h1 className="banner-title">INCONTROLABLES</h1>
         <div className="banner-buttons">
           <button className="banner-btn play">▶ Reproducir</button>
           <button className="banner-btn info">ℹ Más información</button>
         </div>
-        <p className="banner-description">
-          Ocho atracadores toman rehenes en la Fábrica Nacional de Moneda y Timbre española. Desde el encierro, su líder manipula a la policía para llevar a cabo un ambicioso plan.
-        </p>
       </div>
     </section>
   );
