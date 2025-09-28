10ce2951e91030798fb6b9c43cbfa4f140741b1b
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-09-28 09:10:29 -0300

    version 23

--- src/assets/user.jsx
+++ src/assets/user.jsx
@@ -1,20 +1,20 @@
-export function TablerUser(props) {
+export default function TablerUser({ imgUrl, alt }) {
   return (
-      <a
-        href="/YourAccount"
-        role="button"
-        tabIndex={0}
-        aria-haspopup="true"
-        aria-expanded="false"
-        aria-label="Cuenta y configuración"
-      >
-        <span className="profile-link" role="presentation">
-          <img
-            className="profile-icon"
-            src={imgUrl}
-            alt={alt || "User profile"}
-          />
-        </span>
-      </a>
+    <a
+      href="/YourAccount"
+      role="button"
+      tabIndex={0}
+      aria-haspopup="true"
+      aria-expanded="false"
+      aria-label="Cuenta y configuración"
+    >
+      <span className="profile-link" role="presentation">
+        <img
+          className="profile-icon"
+          src={imgUrl}
+          alt={alt || "User profile"}
+        />
+      </span>
+    </a>
   );
 }
