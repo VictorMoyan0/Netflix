62535537361df8c8b514967cc6f7a46b8a7c3b1e
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-09-28 09:09:32 -0300

    version 22

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -38,7 +38,7 @@ function Navbar() {
             </li>
           </ul>
           <BellIcon />
-          <TablerUser />
+          <TablerUser imgUrl="https://occ-0-1701-1740.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229" />
         </div>
       </nav>
     </header>
