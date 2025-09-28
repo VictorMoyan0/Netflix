1bbb992c6ab48cb6f138e5767961e346b94b72e0
VictorMoyan0 <victormoyanosolla@gmail.com>
2025-09-28 10:11:09 -0300

    version 39

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,6 @@
-import BellIcon from './assets/bell';
-import NetflixSearchIcon from './assets/search';
-import TablerUser from './assets/user';
+import BellIcon from './assets/Bell';
+import NetflixSearchIcon from './assets/Search';
+import TablerUser from './assets/User';
 
 function Navbar() {
   const userAvatar = "https://occ-0-1701-1740.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABTZ2zlLdBVC05fsd2YQAR43J6vB1NAUBOOrxt7oaFATxMhtdzlNZ846H3D8TZzooe2-FT853YVYs8p001KVFYopWi4D4NXM.png?r=229";
